87d72ef0bef1ad1c5ed7a2924ce223abc57bea65
Ohm <puwnautladplee>
2024-05-07 12:56:37 +0700

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -17,7 +17,6 @@ server.listen(APP_PORT, () => {
 });
 
 const mongoose = require('mongoose');
-console.log(process.env.DB_USERNAME);
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(uri).then(() => {
     console.log('connected')
